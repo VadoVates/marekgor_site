fd222c6504308648c75f81e3e451429ff983967c
VadoVates <marekgor@gmail.com>
2025-01-02 19:50:49 +0100

    Django prepared

--- marekgor/views.py
+++ marekgor/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
+from marekgor.forms import ContactForm
 from marekgor.models import Marek
 
 
@@ -9,9 +11,27 @@ def index(request):
 def about (request):
     return render (request, 'about.html')
 
-def contact(request):
-    return render (request, 'contact.html')
-
 def marek(request):
     mareks = Marek.objects.all()
     return render(request, 'marek_list.html', {'mareks': mareks})
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            send_mail(
+                subject='Formularz kontaktowy',
+                message=f'Nazwa: {name}\nE-mail: {email}\n\n{message}',
+                from_email=email,
+                recipient_list=['contact@marekgor.com'],
+            )
+
+            return render(request, 'contact_success.html', {'form': form})
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact.html', {'form': form})
