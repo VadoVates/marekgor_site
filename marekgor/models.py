fd13478b7d8039b2c26a945d1b81d5dd0612337e
VadoVates <marekgor@gmail.com>
2025-01-02 18:26:19 +0100

    Template ready.

--- marekgor/models.py
+++ marekgor/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Marek(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
--- marekgor/views.py
+++ marekgor/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from marekgor.models import Marek
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def about (request):
+    return render (request, 'about.html')
+
+def contact(request):
+    return render (request, 'contact.html')
+
+def marek(request):
+    mareks = Marek.objects.all()
+    return render(request, 'marek_list.html', {'mareks': mareks})
