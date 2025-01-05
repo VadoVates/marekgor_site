989eb9d1a542b85579f14a2f87d9526f5a8700cf
VadoVates <marekgor@gmail.com>
2025-01-05 17:23:49 +0100

    Debugging prod contact form error

--- marekgor/views.py
+++ marekgor/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
 
 from marekgor.forms import ContactForm
 from marekgor.models import Marek
 
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
@@ -19,17 +21,21 @@ def contact(request):
             email = form.cleaned_data['email']
             message = form.cleaned_data['message']
 
-            send_mail(
-                subject='Formularz kontaktowy',
-                message=f'Nazwa: {name}\nE-mail: {email}\n\n{message}',
-                from_email=email,
-                recipient_list=['contact@marekgor.com'],
-            )
-
+            try:
+                send_mail(
+                    subject='Formularz kontaktowy',
+                    message=f'Nazwa: {name}\nE-mail: {email}\n\n{message}',
+                    from_email=email,
+                    recipient_list=['contact@marekgor.com'],
+                )
+            except BadHeaderError:
+                return HttpResponse('Invalid header found')
+            except Exception as e:
+                print(f"Email sending failed: {e}")
+                return HttpResponse(f'Error sending email: {e}')
             return render(request, 'contact_success.html', {'form': form})
     else:
         form = ContactForm()
-
     return render(request, 'contact.html', {'form': form})
 
 def marek_list(request):
