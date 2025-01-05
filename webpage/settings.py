f1111c417b5fac97d596d3be50126e4783647b51
VadoVates <marekgor@gmail.com>
2025-01-05 18:21:38 +0100

    Uncommenting sessions and messages to see what crashed site

--- webpage/settings.py
+++ webpage/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
 #    'django.contrib.admin',
 #    'django.contrib.auth',
     'django.contrib.contenttypes',
-#    'django.contrib.sessions',
-#    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'marekgor.apps.MarekgorConfig',
 ]
@@ -46,11 +46,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'marekgor.middleware.BlockSuspiciousPathsMiddleware',
-#    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
 #    'django.contrib.auth.middleware.AuthenticationMiddleware',
-#    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
