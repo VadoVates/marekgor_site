af885da6da304c1027b878ff53bdab2308dc4104
VadoVates <marekgor@gmail.com>
2025-01-06 19:49:33 +0100

    Trying captcha v3 forms.py + contact.html script + views.py

--- marekgor/views.py
+++ marekgor/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
 from marekgor.forms import ContactForm
+import requests
+from django.conf import settings
 
 def index(request):
     return render(request, 'index.html')
@@ -8,10 +10,23 @@ def index(request):
 def about (request):
     return render (request, 'about.html')
 
+def verify_recaptcha(token):
+    url = 'https://www.google.com/recaptcha/api/siteverify'
+    data = {
+        'secret': settings.RECAPTCHA_PRIVATE_KEY,
+        'response': token
+    }
+    response = requests.post(url, data=data)
+    result = response.json()
+    return result.get('success', False)
+
 def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
-        if form.is_valid():
+        recaptcha_token = request.POST.get('g-recaptcha-response')
+        recaptcha_valid = verify_recaptcha(recaptcha_token)
+
+        if form.is_valid() and recaptcha_valid:
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
             message = form.cleaned_data['message']
@@ -23,6 +38,9 @@ def contact(request):
                 recipient_list=['contact@marekgor.com'],
             )
             return render(request, 'contact_success.html', {'form': form})
+        else:
+            if not recaptcha_valid:
+                form.add_error('captcha', 'Error verifying reCAPTCHA, please try again.')
     else:
         form = ContactForm()
     return render(request, 'contact.html', {'form': form})
