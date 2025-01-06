1f99dcdb9fad73c2b534463f581f5b12c11082d5
VadoVates <marekgor@gmail.com>
2025-01-06 19:36:25 +0100

    Trying captcha v3, only in forms.py

--- marekgor/forms.py
+++ marekgor/forms.py
@@ -1,21 +1,14 @@
 from django import forms
 from django_recaptcha.fields import ReCaptchaField
-from django_recaptcha.widgets import ReCaptchaV2Checkbox
+from django_recaptcha.widgets import ReCaptchaV3
 from webpage import settings
 
 class ContactForm(forms.Form):
-    captcha = ReCaptchaField(
-        public_key=settings.RECAPTCHA_PUBLIC_KEY,
-        private_key=settings.RECAPTCHA_PRIVATE_KEY,
-        widget=ReCaptchaV2Checkbox(
-            attrs={
-            'data-theme' : 'dark',
-            'data-callback' : 'onSubmitCallback',
-            'data-action' : 'submit',
-            'data-size' : 'compact',
-            }
-        )
-    )
     name = forms.CharField(max_length=100, label="Your name:")
     email = forms.EmailField(label="Your email address:")
     message = forms.CharField(widget=forms.Textarea, label="Your message:")
+    captcha = ReCaptchaField(
+        public_key=settings.RECAPTCHA_PUBLIC_KEY,
+        private_key=settings.RECAPTCHA_PRIVATE_KEY,
+        widget=ReCaptchaV3 (required_score=0.8)
+    )
