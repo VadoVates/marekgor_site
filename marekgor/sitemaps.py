788a571679d2a7615c23a7b52c4091d054695d74
VadoVates <marekgor@gmail.com>
2025-01-21 18:17:40 +0100

    reformated index, changed headers style, created robots.txt, sitemap.xml, sitemaps code

---
+++ marekgor/sitemaps.py
@@ -0,0 +1,12 @@
+from django.contrib.sitemaps import Sitemap
+from django.shortcuts import reverse
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.8
+    changefreq = 'monthly'
+
+    def items(self):
+        return ['index', 'about', 'contact', 'marek']  # Nazwy widoków z `urls.py`
+
+    def location(self, item):
+        return reverse(item)
--- marekgor/urls.py
+++ marekgor/urls.py
@@ -1,9 +1,16 @@
+from django.contrib.sitemaps.views import sitemap
+from marekgor.sitemaps import StaticViewSitemap
 from django.urls import path
-from . import views
+import views
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('marek/', views.marek, name='marek'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
