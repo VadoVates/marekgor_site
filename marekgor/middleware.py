d333d82ad674672b0696aa4e2d053ec6e9cec225
VadoVates <marekgor@gmail.com>
2025-01-21 18:46:13 +0100

    deleted blocking robots.txt (because I added it); checking out formatting h1 titles

--- marekgor/middleware.py
+++ marekgor/middleware.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseForbidden
 class BlockSuspiciousPathsMiddleware:
     BLOCKED_PATHS = [
         "/wp-admin/", "/wp-content/", "/wp-includes/", "/.git/", "/wp-login.php",
-        "/wp-config.php", "/.gitignore", "/.htaccess", "/readme.html", "/robots.txt",
+        "/wp-config.php", "/.gitignore", "/.htaccess", "/readme.html",
         "/wp-json/", "/wp-cron.php", "/wp-load.php", "/wp-mail.php", "/wp-config-sample.php",
         "/.htpasswd", "/.htaccess.dist", "/.htaccess.sample", "/.htaccess.sample.php",
         "/.htaccess.sample.txt", "/.htaccess.sample.txt.php", "xmlrpc.php", "index.php",
@@ -12,7 +12,6 @@ class BlockSuspiciousPathsMiddleware:
         "/.well-known/", "/server-info/", "/config/", "/backup/", "/temp/", "/logs/",
         "/backup/", "/errors/", "/debug/", "/cache/", "/.env", "/__pycache__/",
         "/migrations/", "/.gitmodules", "/manage.py", "/db.sqlite3", "/wordpress/", "/admin/",
-
     ]
 
     def __init__(self, get_response):
